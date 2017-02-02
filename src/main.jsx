99b0c6c5ec4ef6d777258fc80b755fd5aaebabdf
Ryan <ryan@burgettweb.net>
2017-02-01 20:40:45 -0500

    Removed dist folder

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="container-fluid">
                 <h1>Hi!</h1>
             </div>
         );
