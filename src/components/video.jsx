5d2c6b13cced73a0515c917786e01f7ed7e3928f
Ryan <ryan@burgettweb.net>
2017-02-03 10:40:01 -0500

    Scrolled to top when video page opened

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,48 +1,56 @@
 import React from 'react';
 
-const VideoComponent = ({ id, videos }) => {
+class VideoComponent extends React.Component {
 
-    let video = videos.find(v => v.id === id);
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
 
-    video = video ? video : {};
+    render() {
+        const { id, videos } = this.props;
+        let video = videos.find(v => v.id === id);
 
-    const fileName = video.fileName ? video.fileName : '';
-    const mp4Path = window.s3BucketAddress + fileName.replace(/\.flv$/, '.mp4');
+        video = video ? video : {};
 
-    return (
-        <div style={{marginBottom: 10}}>
-            <div className="row">
-                <div className="col-sm-12" style={{marginBottom: 20}}>
-                    <h4 className="text-center">{video.title}</h4>
+        const fileName = video.fileName ? video.fileName : '';
+        const mp4Path = window.s3BucketAddress + fileName.replace(/\.flv$/, '.mp4');
+
+        return (
+            <div style={{marginBottom: 10}}>
+                <div className="row">
+                    <div className="col-sm-12" style={{marginBottom: 20}}>
+                        <h4 className="text-center">{video.title}</h4>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-                    <div className="embed-responsive embed-responsive-4by3">
-                        <video className="embed-responsive-item" src={mp4Path} controls={true} ></video>
+                <div className="row">
+                    <div className="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+                        <div className="embed-responsive embed-responsive-4by3">
+                            <video className="embed-responsive-item" src={mp4Path} controls={true} ></video>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <p className="text-center">{video.description}</p>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <p className="text-center">
+                            <a href={window.s3BucketAddress + fileName}><span className="glyphicon glyphicon-download"></span> Download hi-res .flv</a>
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <p className="text-center">
+                            <a href={mp4Path}><span className="glyphicon glyphicon-download"></span> Download low-res .mp4</a>
+                        </p>
                     </div>
                 </div>
             </div>
-            <div className="row">
-                <p className="text-center">{video.description}</p>
-            </div>
-            <div className="row">
-                <div className="col-sm-12">
-                    <p className="text-center">
-                        <a href={window.s3BucketAddress + fileName}><span className="glyphicon glyphicon-download"></span> Download hi-res .flv</a>
-                    </p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-sm-12">
-                    <p className="text-center">
-                        <a href={mp4Path}><span className="glyphicon glyphicon-download"></span> Download low-res .mp4</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+
+}
 VideoComponent.propTypes = {
     id: React.PropTypes.string,
     videos: React.PropTypes.arrayOf(React.PropTypes.object)
